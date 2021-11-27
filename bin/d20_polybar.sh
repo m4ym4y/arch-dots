b053e139c8589da3108b3ed99d2a55b7cb6afdcf
m4ym4y <m4ym4y@users.noreply.github.com>
2021-11-26 17:50:01 -0800

    feat: d20 reset on rclick

--- bin/d20_polybar.sh
+++ bin/d20_polybar.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-sleep_pid=0
 
-rerun() {
-  if [ "$sleep_pid" -ne 0 ]; then
-    echo $(expr $RANDOM % 20 + 1)
-    kill $sleep_pid &>/dev/null
-  fi
+roll() {
+  echo $(expr $RANDOM % 20 + 1)
 }
 
-trap "rerun" USR1
+write20() {
+  echo "20"
+}
 
-echo d20
+trap "roll" USR1
+trap "write20" HUP
+
+write20
 while true; do
-  bash -c "sleep 3 && echo d20 && sleep infinity" &
-  sleep_pid=$!
+  sleep infinity &
   wait
 done
